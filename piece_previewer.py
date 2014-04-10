2a90221321300370637a363647d95ceed2af48cc
Adam Reese <adamnreese@gmail.com>
2014-04-10 10:23:46 -0400

    Piece previewer works for first piece. Need to have draw called more.

--- piece_previewer.py
+++ piece_previewer.py
@@ -1,5 +1,6 @@
 from constants import *
 import pygame
+from tetrominos import RandomTetrominoGenerator
 
 class PiecePreviewer:
     def __init__(self, screen):
@@ -8,6 +9,8 @@ class PiecePreviewer:
         self.y = 140
         self.surface = pygame.Surface((PREVIEWER_WIDTH, PREVIEWER_HEIGHT))
         self.surface.fill(PREVIEWER_BGCOLOR)
+	self.generator = RandomTetrominoGenerator()
 
     def draw(self):
         self.screen.blit(self.surface, (self.x, self.y))
+	self.generator.sequence[-1].render(self.surface)
